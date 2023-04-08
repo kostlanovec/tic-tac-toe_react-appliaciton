529e9e530aa7541761fc16e251369c13f0d32dcd
kostik <jansebastiankostlan@gmail.com>
2023-04-08 17:23:43 +0200

    zkouska react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+export default function Square() {
+  return (
+      <>
+  <button className="square">X</button>
+  <button className="square">X</button>
+      </>
+  )
+}
+
